654b19f114c4708fc459510217dff6b0776f12d7
koushikb2002 <96101163+koushikb2002@users.noreply.github.com>
2023-08-02 10:27:23 +0530

    Add files via upload

---
+++ api/models/Student.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+
+const StudentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    comments: {
+      type: [commentSchema], // Store comments as an array of comment objects
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+
+module.exports = mongoose.model("Student", StudentSchema);
